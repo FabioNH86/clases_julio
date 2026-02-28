fb52f34a1211fbd75c238efae8123bb613afe57b
Fabio Noriega <fabionoriega86@icloud.com>
2026-02-28 00:26:36 +0000

    h

---
+++ Fase_3/04_reloj_preciso.py
@@ -0,0 +1,8 @@
+# RETO: Solo la Hora
+# Instrucciones: No queremos todo el módulo datetime, solo la clase 'datetime'.
+# Úsala para imprimir la hora actual.
+
+# TODO: from datetime import datetime
+# ahora = datetime.now()
+
+print(f"Hora exacta: {ahora.strftime('%H:%M:%S')}")
