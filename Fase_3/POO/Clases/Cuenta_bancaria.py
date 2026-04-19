0318823bb484eaabe3f5d58823fe46d0e8e9c83d
ulopo034 <accelmage3@gmail.com>
2026-04-18 19:08:34 -0600

    a

--- Fase_3/POO/Clases/Cuenta_bancaria.py
+++ Fase_3/POO/Clases/Cuenta_bancaria.py
@@ -2,10 +2,32 @@
 Crea una clase llamada CuentaBancaria, que tenga el método depositar(), 
 retirar() y los atributos titular y saldo.
 """
-
-
+class CuentaBancaria():
+    def __init__(self,titular,saldo):
+        self.titular = titular
+        self.saldo = saldo
+    def depositar(self,deposito):
+        self.saldo += deposito
+        print(self.saldo)
+    def retirar(self,retiro):
+        self.saldoinicial = self.saldo
+        self.saldo -= retiro
+        if self.saldo >0:
+            print(self.saldo)
+        else:
+            print("error")
+            self.saldo = self.saldoinicial
+# poner el error cuando el valor sea negativo y que guarde el saldo q se puso
 # Prueba el código:
-mi_cuenta = CuentaBancaria("Julio", 100)
-mi_cuenta.depositar(50)
+mi_cuenta = CuentaBancaria("Julio",100)
+print(mi_cuenta.titular)
+print(mi_cuenta.saldo)
+#print(mi_cuenta.depositar(deposito=50))
+mi_cuenta.depositar(deposito=50)
+
+#print(mi_cuenta)
 mi_cuenta.retirar(80)
-mi_cuenta.retirar(100) # Esto debería mostrar el error
+#print(mi_cuenta)
+mi_cuenta.retirar(retiro=100) # Esto debería mostrar el error
+#print(mi_cuenta)
+mi_cuenta.depositar(deposito=10)
--- Fase_3/POO/Clases/rectangulo.py
+++ Fase_3/POO/Clases/rectangulo.py
@@ -2,8 +2,16 @@
 Crea una clase llamada Rectangulo, que tenga el método area() y perimetro(), además de los atributos base y altura
 """
 # El método debe imprimir "Hola, mi nombre es {self.nombre}."
-
-
+class Rectangulo():
+    def __init__(self,base,altura):
+        self.base = base
+        self.altura = altura
+    def area(self):
+        return self.base * self.altura
+    def perimetro(self):
+        return self.base * 2 + self.altura * 2
+        
+        
 # Uso
 mi_rectangulo = Rectangulo(10, 5)
 print(f"Área: {mi_rectangulo.area()}")
